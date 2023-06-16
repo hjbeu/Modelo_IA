160206cff4cfc9f526c95c485936cf05198e70e6
hjbeu <joao.helder@raio.tech>
2023-06-16 09:37:00 -0300

    Remoção de comentários

--- src/app/page.js
+++ src/app/page.js
@@ -35,26 +35,6 @@ export default function Home() {
   };
 
   const [data, setData] = useState({});
-  // const [prob, setProb] = useState({});
-  // eu faria um [data, setData] pra por os 2 juntos, eles vem do mesmo endpoint mesmo
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await fetch('http://localhost:8000/result');
-  //     const data = await response.json();
-  //     setData(data)
-
-  //   })();
-  // }, []);
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await fetch('http://localhost:8000/predict');
-  //     const prob = await response.json();
-  //     setData(prob)
-
-  //   })();
-  // }, []);
-
   const [previewImage, setPreviewImage] = useState('https://img.elo7.com.br/product/original/308E224/placa-decorativa-mdf-cerveja-vc-deve-beber-padawan-star-wars-jason-voorhees.jpg')
   const handleFileChange = (event) => {
     handleFileUpload(event)
@@ -69,7 +49,6 @@ export default function Home() {
   };
 
   useEffect(() => {
-    // Personalize o scroll aqui, por exemplo:
     const scrollbarStyles = `
       ::-webkit-scrollbar {
         width: 10px;
@@ -88,7 +67,6 @@ export default function Home() {
     styleElement.innerHTML = scrollbarStyles;
     document.head.appendChild(styleElement);
     
-    // Remova o estilo quando o componente for desmontado
     return () => {
       document.head.removeChild(styleElement);
     };
@@ -98,20 +76,18 @@ export default function Home() {
     <main className=" flex min-h-screen flex-col items-center justify-center p-2" >
       <Image className="absolute z-0 w-screen h-screen object-cover" src={fundo} alt="fundo" />
       <div className="flex bg-opacity-70  backdrop-blur-lg shadow-lg flex-col items-center justify-around rounded-lg mb-4 p-6">
-        <h1 className='text-justify text-4xl p-4 pb-10'>Modelo de classificação de imagem com a base CIFR10</h1>
+        <h1 className='text-justify text-4xl text-white p-4 pb-10'>Modelo de classificação de imagem com a base CIFR10</h1>
         <div className='flex items-center justify-between gap-4'>
           <div className='flex flex-col items-center justify-center gap-4'>
             <img src={previewImage} alt="Picture of the author" width={400} height={400} />
           </div>
           <div className='flex flex-col  justify-center gap-4' >
-            <p className='text-xl'>Resultado: {data.resu}</p>
-            <div className='overflow-auto' style={{ maxHeight: '400px' }}>{/* <p className='text-xl'>Probabilidade: {data.prob}</p> */}
+            <p className='text-xl text-white'>Resultado: {data.resu}</p>
+            <div className='overflow-auto' style={{ maxHeight: '400px' }}>
             {data.prob && Object.entries(data.prob).map(([key, value]) => (
               <div key={key}>
-                <p className='text-lg'>Classe: {key}</p>
-                
-                <p className='text-lg'>Probabilidade: {value[0]}</p>
-                
+                <p className='text-lg text-white'>Classe: {key}</p>
+                <p className='text-lg text-white' >Probabilidade: {value[0]}</p>
               </div>
             ))}</div>
             <form onSubmit={submitFile} >
@@ -119,9 +95,10 @@ export default function Home() {
                 <div className="flex items-center justify-center">
                   <input id="predict" className="flex items-center px-4 py-2 bg-blue-500 text-white text-sm rounded-md border border-blue-700 hover:bg-blue-700 cursor-pointer" type="file" onChange={(e) => handleFileChange(e)} />
                 </div>
-                <button type="submit" className='bg-green-800 hover:bg-green-700  p-3 rounded-lg w-full'>Upload</button>
+                <button type="submit" className='bg-green-800 hover:bg-green-700 text-white p-3 rounded-lg w-full'>Upload</button>
               </div>
-            </form></div>
+            </form>
+            </div>
         </div>
       </div>
     </main>
